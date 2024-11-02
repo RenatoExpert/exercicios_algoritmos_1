e888af9c329e17ac08e1cfd16e371ebfb43480d3
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-11-02 19:55:45 -0300

    making more testes and rules

--- moneypot/main.c
+++ moneypot/main.c
@@ -27,10 +27,27 @@ bool rule1 (struct OrderRequest request) {
 	return pass;
 }
 
+bool rule2 (struct OrderRequest request) {
+	bool pass = request.requester -> isGoodPayer;
+	return pass;
+}
+
+bool rule3 (struct OrderRequest request) {
+	bool pass = request.requester -> hasStability;
+	return pass;
+}
+
+bool rule4 (struct OrderRequest request) {
+	bool pass = request.requestedValue < 10 * request.entryValue;
+	return pass;
+}
+
 struct OrderResponse requestLoan (struct OrderRequest request) {
 	bool r1 = rule1(request);
-	struct Customer *customer = request.requester;
-	bool approved = r1;
+	bool r2 = rule2(request);
+	bool r3 = rule3(request);
+	bool r4 = rule4(request);
+	bool approved = r1 && r2 && r3 && r4;
 	struct OrderResponse response = { &request, approved };
 	return response;
 }
@@ -42,11 +59,18 @@ void test1 () {
 	bool isGoodPayer = true;
 	bool hasStability = true;
 	struct Customer client1 = { name, incomePerMonth, isGoodPayer, hasStability };
-	int requestedValue = 30000;
-	int entryValue = 3000;
-	struct OrderRequest loan1 = { requestedValue, entryValue, &client1 };
-	struct OrderResponse response = requestLoan(loan1);
-	assert(response.approved);
+	//	Subtest 1
+	int requestedValue1 = 30000;
+	int entryValue1 = 3500;
+	struct OrderRequest loan1 = { requestedValue1, entryValue1, &client1 };
+	struct OrderResponse response1 = requestLoan(loan1);
+	assert(response1.approved);
+	//	Subtest 2
+	int requestedValue2 = 30000;
+	int entryValue2 = 2500;
+	struct OrderRequest loan2 = { requestedValue2, entryValue2, &client1 };
+	struct OrderResponse response2 = requestLoan(loan2);
+	assert(!response2.approved);
 	printf("OK \n");
 }
 
