e29c82ad2ce0b1bc76b4343fcd71a20643c1eb38
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-11-30 22:49:06 -0300

    use pointers instead of malloc

--- alunos/main.c
+++ alunos/main.c
@@ -25,18 +25,18 @@ void gerarAluno (struct Aluno * pointer, char *nome, float media_a, float media_
 	*pointer = aluno;
 }
 
-void classificarAlunos(struct Aluno * alunos, int howManyStudents) {
-	for (int position = 1; position <= sizeof(*alunos); position++) {
+void classificarAlunos (struct Aluno alunos[], int howManyStudents) {
+	for (int position = 1; position <= howManyStudents; position++) {
 		int thisTurnBestGuy;
 		int thisTurnBestScore = 0;
 		for (int aluno_index = 0; aluno_index < howManyStudents; aluno_index++) {
+			printf(" aluno_index = %i ", aluno_index);
 			struct Aluno aluno = alunos[aluno_index];
-			//printf(" aluno_index = %i ", aluno_index);
+			printf(" aluno.media_a = %f \n", aluno -> media_a);
 			if (aluno.classificacao == 0) {
-				printf(" aluno.media_a = %f ", aluno.media_a);
 				if (aluno.media_geral > thisTurnBestScore) {
 					thisTurnBestGuy = aluno_index;
-					printf("<< thisTurnBestGuy = %i , aluno_index = %i >>",thisTurnBestGuy, aluno_index);
+					//printf("<< thisTurnBestGuy = %i , aluno_index = %i >>",thisTurnBestGuy, aluno_index);
 				}
 			}
 		}
@@ -57,20 +57,21 @@ void test1 () {
 	float media_geral = 4.9;
 	assert(aluno1 -> media_a == media_a);
 	assert(aluno1 -> nome == nome);
-	//	This is giving some erros. media_geral from function and from literal are not being the same
+	//	This line is giving some erros. media_geral from function and from literal are not being the same
 	//assert(aluno1 -> media_geral == media_geral);
 	//printf("%b => %b", aluno1 -> media_geral, media_geral);
 	printf(" OK \n");
 }
 
-
 void test2 () {
 	printf("Test 2 - ");
 	int howManyStudents = 3;
-	struct Aluno * alunos = malloc(howManyStudents * sizeof(struct Aluno));
+	//struct Aluno * alunos = malloc(howManyStudents * sizeof(struct Aluno));
+	struct Aluno alunos[howManyStudents];
 	struct Aluno aluno1 = alunos[0];
 	struct Aluno aluno2 = alunos[1];
 	struct Aluno aluno3 = alunos[2];
+	printf("Array %p Ponteiros %p %p %p\n", &alunos, &aluno1, &aluno2, &aluno3);
 	gerarAluno(&aluno1, "Roberto", 5.6, 4.2);
 	gerarAluno(&aluno2, "Erick", 5.2, 4.8);
 	gerarAluno(&aluno3, "Henrique", 3.2, 8.8);
